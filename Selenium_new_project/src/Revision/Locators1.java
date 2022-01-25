d6e98a71c3b1607193de8d1882f3a68b00030471
RISHABH SRIVASTAVA <72760884+rishabhnextgen@users.noreply.github.com>
2022-01-25 19:31:41 +0530

    new package
    new added

---
+++ Selenium_new_project/src/Revision/Locators1.java
@@ -0,0 +1,21 @@
+package Revision;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "F:\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://practice.automationtesting.in/");
+		driver.manage().window().maximize();
+		
+	
+		
+		
+		
+	}
+
+}
